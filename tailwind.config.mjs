7032dca3e2208abd46d881facc8f0654363ae0ae
Santiago Daglio <santidaglio@gmail.com>
2024-03-05 18:34:47 -0300

    adding all project files

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+import animations from '@midudev/tailwind-animations'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: "class",
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		"./node_modules/flowbite/**/*.js"
+	],
+	theme: {
+		extend: {
+			colors: {
+				'light-primary': '#fff9e6',
+				'light-secondary': '#fff2cc',
+				'light-tertiary': '#ffecb3',
+				'dark-primary': '#29272d'
+			}
+		},
+	},
+	 plugins: [animations]
+	 
+}
